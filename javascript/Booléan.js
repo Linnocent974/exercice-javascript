aba65f8bcfd68a3041e4c499a2d89b63179cc967
unknown <larcange974@gmail.com>
2022-09-03 16:26:30 +0200

    boolean

---
+++ javascript/Booléan.js
@@ -0,0 +1,20 @@
+//Les type de données [booléan] sert a manipuler les valeur vrai et faux [true et false]
+
+console.log(true, false);
+
+let email = 'email@gmail.com';
+
+console.log(email.includes('@'));
+console.log(email.includes('x'));
+
+//oprérateur de comparaison [==, !=, >, <, >=, <= ]
+
+let age = 25;
+let nom = 'coutie';
+
+console.log(age == 25);
+console.log(age != 25);
+console.log(age > 25);
+console.log(age < 25);
+console.log(age >= 25);
+console.log(age <= 25);
