ab01cda4e3f4296f6e70d38a7bdf2a4e0bc458f5
unknown <larcange974@gmail.com>
2022-12-21 17:57:49 +0100

    Math object

---
+++ javascript/math object.js
@@ -0,0 +1,23 @@
+//Math
+console.log(Math);
+
+//Accés aux methodes liées au Math object
+console.log(Math.PI);
+
+const value = 8.1;
+
+//Arrondissement par excés ou par défaut
+console.log(Math.round(value));
+
+//conversion d'un nombre décimal entier 
+console.log(Math.floor(value));
+
+// Arrondissement par excés
+console.log(Math.ecil(value));
+
+//Random numbers (Génération d'un nombre aléatoire)
+const random = Math.random();
+console.log(random);
+
+//Personnaliser un générateur de nombre aléatoire, ex: 0-100
+console.log(Math.round(random * 1000));
